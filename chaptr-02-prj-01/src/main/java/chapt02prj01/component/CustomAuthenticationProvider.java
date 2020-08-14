e73212dd61694ec89020bfdf4a3d185052af5a5c
Jiri.Hermann <jiri.hermann@adastragrp.com>
2020-08-14 12:58:46 +0200

    prj 5

---
+++ chaptr-02-prj-01/src/main/java/chapt02prj01/component/CustomAuthenticationProvider.java
@@ -0,0 +1,28 @@
+package chapt02prj01.component;
+
+import org.springframework.security.authentication.AuthenticationCredentialsNotFoundException;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.stereotype.Component;
+
+import java.util.Arrays;
+
+@Component
+public class CustomAuthenticationProvider implements AuthenticationProvider {
+    @Override
+    public Authentication authenticate(Authentication authentication) throws AuthenticationException {
+        String user = authentication.getName();
+        String password = String.valueOf(authentication.getCredentials());
+        if("john".equals(user) && "1234".equals(password)){
+            return new UsernamePasswordAuthenticationToken(user,password, Arrays.asList());
+        }
+        throw new AuthenticationCredentialsNotFoundException("Error in authentication");
+    }
+
+    @Override
+    public boolean supports(Class<?> aClass) {
+        return UsernamePasswordAuthenticationToken.class.isAssignableFrom(aClass);
+    }
+}
--- chaptr-02-prj-01/src/main/java/chapt02prj01/config/ProjectConfig.java
+++ chaptr-02-prj-01/src/main/java/chapt02prj01/config/ProjectConfig.java
@@ -1,29 +1,28 @@
 package chapt02prj01.config;
 
-import org.springframework.context.annotation.Bean;
+import chapt02prj01.component.CustomAuthenticationProvider;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.NoOpPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @Configuration
-public class ProjectConfig {
-//    @Bean
-//    public UserDetailsService userDetailsService(){
-//        var userDetailService = new InMemoryUserDetailsManager();
-//        var user = User.withUsername("john")
-//                .password("1234")
-//                .authorities("read")
-//                .build();
-//        userDetailService.createUser(user);
-//        return  userDetailService;
-//    }
-//    @Bean
-//    public PasswordEncoder passwordEncoder(){
-//        return NoOpPasswordEncoder.getInstance();
-//    }
+public class ProjectConfig extends WebSecurityConfigurerAdapter {
 
+    private final CustomAuthenticationProvider customAuthenticationProvider;
+
+    public ProjectConfig(CustomAuthenticationProvider customAuthenticationProvider) {
+        this.customAuthenticationProvider = customAuthenticationProvider;
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.authenticationProvider(customAuthenticationProvider);
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.httpBasic();
+        http.authorizeRequests().anyRequest().authenticated();
+    }
 }
