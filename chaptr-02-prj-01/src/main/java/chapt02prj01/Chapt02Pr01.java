095faf09fd925511121af1ee8f5707b728092b67
hermison <hermann.jirka@gmail.com>
2020-08-14 10:32:40 +0200

    create repo

---
+++ chaptr-02-prj-01/src/main/java/chapt02prj01/Chapt02Pr01.java
@@ -0,0 +1,11 @@
+package chapt02prj01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Chapt02Pr01 {
+    public static void main(String[] args) {
+        SpringApplication.run(Chapt02Pr01.class, args);
+    }
+}
