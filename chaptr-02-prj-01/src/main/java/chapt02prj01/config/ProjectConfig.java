aff7552dc639b8f88a54c8a5c1083ffdf71d911b
hermison <hermann.jirka@gmail.com>
2020-08-17 13:15:17 +0200

    chapter 5 prj 03

--- chaptr-02-prj-01/src/main/java/chapt02prj01/config/ProjectConfig.java
+++ chaptr-02-prj-01/src/main/java/chapt02prj01/config/ProjectConfig.java
@@ -1,45 +1,18 @@
 package chapt02prj01.config;
 
-import org.springframework.beans.factory.InitializingBean;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.scheduling.annotation.EnableAsync;
-import org.springframework.security.authentication.AuthenticationProvider;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.crypto.password.NoOpPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.JdbcUserDetailsManager;
-
-import javax.sql.DataSource;
 
 @Configuration
 public class ProjectConfig extends WebSecurityConfigurerAdapter {
 
-    @Autowired
-    private AuthenticationProvider authenticationProvider;
-
-    @Bean
-    public UserDetailsService userDetailsService(DataSource dataSource) {
-        return new JdbcUserDetailsManager(dataSource);
-    }
-
-//    @Bean
-//    public InitializingBean initializingBean() {
-//        return () -> SecurityContextHolder.setStrategyName(
-//                SecurityContextHolder.MODE_INHERITABLETHREADLOCAL);
-//    }
-
-    @Bean
-    public PasswordEncoder passwordEncoder() {
-        return NoOpPasswordEncoder.getInstance();
-    }
-
     @Override
-    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.authenticationProvider(authenticationProvider);
+    protected void configure(HttpSecurity http) throws Exception {
+        http.httpBasic(c -> {
+            c.realmName("OTHER");
+            c.authenticationEntryPoint(new CustomEntryPoint());
+        });
+        http.authorizeRequests().anyRequest().authenticated();
     }
 }
--- chaptr-02-prj-01/src/main/java/chapt02prj01/controllers/HelloController.java
+++ chaptr-02-prj-01/src/main/java/chapt02prj01/controllers/HelloController.java
@@ -1,16 +1,10 @@
 package chapt02prj01.controllers;
 
-import org.springframework.security.concurrent.DelegatingSecurityContextCallable;
-import org.springframework.security.concurrent.DelegatingSecurityContextExecutorService;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContext;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.concurrent.Callable;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
 
 @RestController
 public class HelloController {
@@ -20,42 +14,4 @@ public class HelloController {
         Authentication authentication = context.getAuthentication();
         return "Hello: " + authentication.getName();
     }
-
-    @GetMapping("/bey")
-    public String goodBay() {
-        SecurityContext context = SecurityContextHolder.getContext();
-        Authentication authentication = context.getAuthentication();
-        return "Goodbay: " + authentication.getName();
-    }
-
-    @GetMapping("/caio")
-    public String ciao() throws Exception {
-        Callable<String> task = () -> {
-            SecurityContext context = SecurityContextHolder.getContext();
-            return context.getAuthentication().getName();
-        };
-        ExecutorService ex = Executors.newCachedThreadPool();
-        try {
-            var contextTask = new DelegatingSecurityContextCallable<>(task);
-            return "CIao: " + ex.submit(contextTask).get() + "!";
-        } finally {
-            ex.shutdown();
-        }
-    }
-
-    @GetMapping("/hola")
-    public String hola() throws Exception {
-        Callable<String> task = () -> {
-            SecurityContext context = SecurityContextHolder.getContext();
-            return context.getAuthentication().getName();
-        };
-        ExecutorService ex = Executors.newCachedThreadPool();
-        try {
-             ex = new DelegatingSecurityContextExecutorService(ex);
-            return "<Hola>: " + ex.submit(task).get() + "!";
-        } finally {
-            ex.shutdown();
-        }
-    }
-
 }
