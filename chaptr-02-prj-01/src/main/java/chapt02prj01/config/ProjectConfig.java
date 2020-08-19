e8e1e205f3c7a8eff7eae059ace19f85c86f7e24
hermison <hermann.jirka@gmail.com>
2020-08-19 10:19:37 +0200

    07 01

--- chaptr-02-prj-01/src/main/java/chapt02prj01/config/ProjectConfig.java
+++ chaptr-02-prj-01/src/main/java/chapt02prj01/config/ProjectConfig.java
@@ -4,26 +4,34 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @Configuration
 public class ProjectConfig {
-//    @Bean
-//    public UserDetailsService userDetailsService(){
-//        var userDetailService = new InMemoryUserDetailsManager();
-//        var user = User.withUsername("john")
-//                .password("1234")
-//                .authorities("read")
-//                .build();
-//        userDetailService.createUser(user);
-//        return  userDetailService;
-//    }
-//    @Bean
-//    public PasswordEncoder passwordEncoder(){
-//        return NoOpPasswordEncoder.getInstance();
-//    }
 
+    @Bean
+    public UserDetailsService userDetailsService() {
+        var manager = new InMemoryUserDetailsManager();
+
+        var usr1 = User.withUsername("jirka")
+                .password("1234")
+                .authorities("read")
+                .build();
+        var usr2 = User.withUsername("pepa")
+                .password("1234")
+                .authorities("write")
+                .build();
+
+        manager.createUser(usr1);
+        manager.createUser(usr2);
+
+        return manager;
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return NoOpPasswordEncoder.getInstance();
+    }
 }
