415a18013e216a05a24f30b8694a925e09d63faf
jiri.hermann <jiri.hermann@adastragrp.com>
2020-09-08 21:25:55 +0200

    ant matcher

--- chaptr-02-prj-01/src/main/java/chapt02prj01/config/WebConfig.java
+++ chaptr-02-prj-01/src/main/java/chapt02prj01/config/WebConfig.java
@@ -1,7 +1,6 @@
 package chapt02prj01.config;
 
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
@@ -11,8 +10,7 @@ public class WebConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http.httpBasic();
         http.authorizeRequests()
-                .mvcMatchers("/product/{code:^[0-9]*$}").permitAll()
-                .anyRequest()
-                .denyAll();
+                .antMatchers("/hello")
+                .authenticated();
     }
 }
--- chaptr-02-prj-01/src/main/java/chapt02prj01/controllers/HelloController.java
+++ chaptr-02-prj-01/src/main/java/chapt02prj01/controllers/HelloController.java
@@ -1,7 +1,5 @@
 package chapt02prj01.controllers;
 
-import org.springframework.security.core.context.SecurityContext;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,22 +8,5 @@ public class HelloController {
     @GetMapping("/hello")
     public String hello() {
         return "Hello";
-    }
-
-    @GetMapping("/ciao")
-    public String ciao() {
-        return "Ciao";
-    }
-
-    @GetMapping("/guten")
-    public String gutenTag() {
-        return "Gutentag";
-    }
-
-    @GetMapping("/hola")
-    public String hola(){
-        SecurityContext context = SecurityContextHolder.getContext();
-        String name = context.getAuthentication().getName();
-        return "Hola "+name;
     }
 }
