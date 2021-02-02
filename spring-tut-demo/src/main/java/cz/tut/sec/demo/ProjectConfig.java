ee83e8024550f4b5297a7a2bd2e4d9c6adf80a9c
hemmermann <hermann.jirka15@gmail.com>
2021-02-02 01:07:59 +0100

    chapt 0504

--- spring-tut-demo/src/main/java/cz/tut/sec/demo/ProjectConfig.java
+++ spring-tut-demo/src/main/java/cz/tut/sec/demo/ProjectConfig.java
@@ -1,44 +1,17 @@
 package cz.tut.sec.demo;
 
-import org.springframework.beans.factory.InitializingBean;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.scheduling.annotation.Async;
 import org.springframework.scheduling.annotation.EnableAsync;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.JdbcUserDetailsManager;
-
-import javax.sql.DataSource;
 
 @Configuration
 @EnableAsync
 public class ProjectConfig {
     @Bean
-    public UserDetailsService userDetailsService(DataSource dataSource) {
-        String usersByUsernameQuery =
-                "select username, password, enabled from users where username = ?";
-        String authsByUserQuery = "select username, authority from spring.authorities where username = ?";
-        var userDetailsManager = new JdbcUserDetailsManager(dataSource);
-        userDetailsManager.setUsersByUsernameQuery(usersByUsernameQuery);
-        userDetailsManager.setAuthoritiesByUsernameQuery(authsByUserQuery);
-        return userDetailsManager;
-    }
-
-    @Bean
     public PasswordEncoder passwordEncoder() {
         return NoOpPasswordEncoder.getInstance();
-    }
-
-    @Bean
-    public InitializingBean initializingBean() {
-        return () -> SecurityContextHolder.setStrategyName(
-                SecurityContextHolder.MODE_INHERITABLETHREADLOCAL);
     }
 }
 
--- spring-tut-demo/src/main/java/cz/tut/sec/demo/WebProjectSecurityConfig.java
+++ spring-tut-demo/src/main/java/cz/tut/sec/demo/WebProjectSecurityConfig.java
@@ -1,20 +1,18 @@
 package cz.tut.sec.demo;
 
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.AuthenticationProvider;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @Configuration
 public class WebProjectSecurityConfig extends WebSecurityConfigurerAdapter {
-    private final AuthenticationProvider authenticationProvider;
-
-    public WebProjectSecurityConfig(AuthenticationProvider authenticationProvider) {
-        this.authenticationProvider = authenticationProvider;
-    }
 
     @Override
-    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.authenticationProvider(authenticationProvider);
+    protected void configure(HttpSecurity http) throws Exception {
+        http.httpBasic(c -> {
+            c.realmName("other");
+        });
+
+        http.authorizeRequests().anyRequest().authenticated();
     }
 }
