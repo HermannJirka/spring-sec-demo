8fb42cb2866a700a67aa4a0cc035d49b5973f032
hemmermann <hermann.jirka15@gmail.com>
2021-02-12 01:17:23 +0100

    chapter 0802

--- spring-tut-demo/src/main/java/cz/tut/sec/demo/HelloController.java
+++ spring-tut-demo/src/main/java/cz/tut/sec/demo/HelloController.java
@@ -3,6 +3,7 @@ package cz.tut.sec.demo;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -17,4 +18,24 @@ public class HelloController {
     public String ciao(Authentication authentication) {
         return "Ciao!: " + authentication.getName();
     }
+
+    @GetMapping("/a")
+    public String getA(){
+        return "works";
+    }
+    @PostMapping("/a")
+    public String postA(){
+        return "works";
+    }
+
+    @GetMapping("/a/b")
+    public String getAB(){
+        return "works";
+    }
+
+    @GetMapping("/a/b/c")
+    public String getABC(){
+        return "works";
+    }
+
 }
--- spring-tut-demo/src/main/java/cz/tut/sec/demo/ProjectConfig.java
+++ spring-tut-demo/src/main/java/cz/tut/sec/demo/ProjectConfig.java
@@ -3,6 +3,7 @@ package cz.tut.sec.demo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -34,9 +35,16 @@ public class ProjectConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http.httpBasic();
 
+
         http.authorizeRequests()
-                .mvcMatchers("/ciao").hasRole("ADMIN")
-                .mvcMatchers("/hello").hasRole("MANAGER");
+                .mvcMatchers(HttpMethod.GET, "/a")
+                .authenticated()
+                .mvcMatchers(HttpMethod.POST, "/a")
+                .permitAll()
+                .anyRequest()
+                .denyAll();
+
+        http.csrf().disable();
     }
 }
 
